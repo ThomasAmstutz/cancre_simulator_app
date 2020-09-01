01c1dc0c710eb330e7641740bd17cf98c6e010b5
Thomas Amstutz <54527950+ThomasAmstutz@users.noreply.github.com>
2020-09-01 14:16:50 +0200

    Modification de la communication

--- lib/screens/gamePage.dart
+++ lib/screens/gamePage.dart
@@ -64,7 +64,7 @@ class _GameState extends State<GamePage> {
 
     socket = widget.socket;
 
-    welcomeReceived();
+    // welcomeReceived();
 
     // Accelerometer events
     _streamSubscriptions
@@ -108,12 +108,6 @@ class _GameState extends State<GamePage> {
     packet.insertAll(0, utf8.encode(packet.length.toString()));
     
     socket.add(packet);
-
-    // socket.write(utf8.encode(clientId.toString()));
-    // socket.write(utf8.encode(widget.nomJoueur));
-    
-    // socket.add(utf8.encode(clientId.toString()));
-    // socket.add(utf8.encode(widget.nomJoueur));
   }
 
   Future _sendClick() async {
