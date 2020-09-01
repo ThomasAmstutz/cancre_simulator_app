a4478404dfafae54218e0c2937f47cb8aace7fa4
Thomas Amstutz <54527950+ThomasAmstutz@users.noreply.github.com>
2020-09-01 12:16:18 +0200

    Update instructionsPage.dart

--- lib/screens/instructionsPage.dart
+++ lib/screens/instructionsPage.dart
@@ -189,7 +189,7 @@ _openConnection() async {
 
   // Réception et traitement du packet
   socket.listen((List<int> event) {
-    _text.text = event.toString().substring(142);
+    _text.text = event.toString().substring(142, 8);
     // int newClientId = event[event.length - 3];
     // String receivedEvent = newClientId.toString();
     // String receivedEvent = utf8.decode(event);
