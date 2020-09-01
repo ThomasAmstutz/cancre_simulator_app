8dcdab0b3b227f21e7dc7e5e79e23d68f45a184f
Thomas Amstutz <54527950+ThomasAmstutz@users.noreply.github.com>
2020-09-01 11:22:16 +0200

    Update instructionsPage.dart

--- lib/screens/instructionsPage.dart
+++ lib/screens/instructionsPage.dart
@@ -189,7 +189,10 @@ _openConnection() async {
 
   // Réception et traitement du packet
   socket.listen((List<int> event) {
-    _text.text = event.toString();
+    // _text.text = event.toString();
+    List<int> eventId;
+    eventId.add(event[event.length - 3]);
+    _text.text = eventId.toString();
     // String receivedEvent = utf8.decode(event);
     // _text.text = receivedEvent;
     // globals.id = event[0];
